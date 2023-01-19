4991bde302d4b54d5f680704ad421576158f2763
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-19 13:25:59 +0100

    Lead items render

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,19 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   console.log(myLeads);
+  inputEl.value = "";
+  renderLeads();
 });
 
-let listItems = "";
-
-for (let i = 0; i < myLeads.length; i++) {
-  const li = document.createElement("li");
-  li.textContent = myLeads[i];
-  ulEl.append(li);
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`;
+  }
+  ulEl.innerHTML = listItems;
 }
